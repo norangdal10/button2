d25fa831ef6b1f8e7cb9ea815708076ab6aa16ad
Munjeong Choi <norangdal10@mju.ac.kr>
2017-11-15 14:20:47 +0900

    birthday

--- button1.py
+++ button1.py
@@ -2,10 +2,12 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="One")
-b2 = Button(window, text="Two")
+b1 = Button(window, text="Choi")
+b2 = Button(window, text="60162183")
+b3 = Button(window, text="1010")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
